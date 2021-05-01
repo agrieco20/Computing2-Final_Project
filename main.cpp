787577b82eee1a7e9e459f19be3ce3bddf17787a
agrieco20 <agrieco896@anselm.edu>
2021-05-01 15:00:12 -0400

    Second Commit (Saturday, 5/1 2021 @ 3pm)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <fstream>
 #include "Applicant.h"
+//#include <string>
 
 using namespace std;
 
@@ -29,6 +30,7 @@ vector <string> indivResume;
 //}
 int counter = 0; //Acts as a counter that is used to increase the current requirement number (only in console output)
 int totalNumPoints = 0; //Used to determine the total number of points possible to be earned by the applicants and used to calculate the percentage match
+int applicantPoints=0;
 bool status; //Acts as a flag used throughout the program
 string userInput; //Used to store all the information passed by the user to the program (ex: skills being looked for in a resume)
 int placeHold; //Used for exception
@@ -42,11 +44,11 @@ int main(){
     string line; //Used to extract each line from file
 
     ifstream infile;
-    infile.open ("Abhishek.txt", ios::in);
-//    infile.open ("Smith Resume.txt", ios::in);
+//    infile.open ("Abhishek.txt", ios::in);
+    infile.open ("Smith Resume.txt", ios::in);
 //    infile.open ("resume v6.txt", ios::in);
 
-    if (infile.is_open()){
+    if (infile.is_open()){ //Opens the individual resume and assigns it to its own
         while(getline(infile, line)){
 //            allResumes.push_back(split(line, ' '))
             vector<string> myvect=split(line, ' ');
@@ -59,7 +61,7 @@ int main(){
     cout<<endl<<endl;
 
 
-    do{
+    do{ //Asks the user for how many points
         try {
             cout<<"How many skills are you looking for your applicants to have? ";
             getline(cin, userInput);
@@ -74,8 +76,7 @@ int main(){
             cin.ignore();
             status=false;
         }
-    }
-    while(status == false);
+    } while(status == false);
 
 //        //cout<<"What is your first requirement? ";
 //        counter++;
@@ -111,7 +112,7 @@ int main(){
 
     }
 
-    applicant.getReq();
+//    applicant.getReq();
 
 //    for(auto elem :allResumes){
 //        for(auto req : applicant.copyReqVect){
@@ -134,7 +135,7 @@ int main(){
 //    }
 
 //    if (applicant.copyReqVect.empty()){
-    if (applicant.copyReqVect.empty()){
+    if (applicant.copyReqVect.empty()){ //NOT PERMANENT
         cout << "Empty" << endl;
     }
 
@@ -153,6 +154,57 @@ int main(){
 //        }
 //    }
 
+    vector<string> multiWord;
+
+    for (int i = 0; i<applicant.copyReqVect.size(); i+=2){//TEST
+        for(auto elem : applicant.copyReqVect[i]){ //Checks to see if the user inputted multiple words and splits them up accordingly
+            if(isspace(elem)){
+                multiWord=split(applicant.copyReqVect[i],' ');
+            }
+        }
+//        if(applicant.copyReqVect[i] ==)
+        for (int j = 0; j<indivResume.size(); j++){
+            if(applicant.copyReqVect[i] == indivResume[j]){
+                //cout << "Found Single Word" << endl; //cout Not Permanent
+                //cout<<applicant.copyReqVect[i+1]<<endl;
+                applicantPoints+=stoi(applicant.copyReqVect[i+1]);
+            }
+            for (int k = 0; k<multiWord.size(); k++){
+//            if (applicant.copyReqVect[i] == indivResume[j]) {
+
+                if(multiWord[k] == indivResume[j] && multiWord[k+1] == indivResume[j+1]){ //Searches for multiple words sent by a user
+                    //cout << "Found Multiple Words" << endl; //cout Not Permanent
+//                    cout<<applicant.copyReqVect[i+1]<<endl;
+                    applicantPoints+=stoi(applicant.copyReqVect[i+1]);
+                    //cout<<applicantPoints<<endl;
+                    multiWord.clear();
+                    break;
+                }
+
+//                //else if(multiWord[k] == indivResume[j]){ //Searches for a single word sent by a user
+//                else if(applicant.copyReqVect[i] == indivResume[j]){
+//                    cout << "Found Single Word" << endl; //cout Not Permanent
+//                    cout<<applicant.copyReqVect[i+1]<<endl;
+//                    applicantPoints+=stoi(applicant.copyReqVect[i+1]);
+//                    multiWord.clear();
+//                }
+
+//                cout << "Found" << endl; //cout Not Permanent
+//                cout<<applicant.copyReqVect[i+1]<<endl;
+//                applicantPoints+=stoi(applicant.copyReqVect[i+1]);
+            }
+        }
+    }
+
+
+
+    //CALCULATIONS
+
+    int divide=100/totalNumPoints;
+    int mult=divide*applicantPoints;
+    cout<<mult<<"% Chance of Being Hired"<<endl;
+
+
 
 
     // applicant.setReq(userInput);
@@ -163,13 +215,11 @@ int main(){
 //        }
 //    }
 
-//    for (int i = 0; i < applicant.copyReqVect.size(); i++){
-    for (int i = 0; i < applicant.copyReqVect.size(); i++){ //NOT PERMANENT
-        cout << applicant.copyReqVect[i] << endl;
-    }
+//    for (int i = 0; i < applicant.copyReqVect.size(); i++){ //NOT PERMANENT
+//        cout << applicant.copyReqVect[i] << endl;
 //    }
 
-    cout << applicant.copyReqVect[1] << endl; //NOT PERMANENT
+//    cout << applicant.copyReqVect[1] << endl; //NOT PERMANENT
 
     infile.close();
     return 0;
@@ -188,11 +238,11 @@ vector<string> split(string line, char delimiter) { //May need to make this a vo
     string word;
     for (int i=0; i<line.size();i++) {
         if (((line[i]) != delimiter) and (i != line.size() - 1)) {
-            word+=line[i];
+            word+=tolower(line[i]); //Sets everything being placed in the resume vector to lowercase (error control)
         }
         else{
             if(i==line.size()-1)
-                word+=line[i];
+                word+=tolower(line[i]); //Sets everything being placed in the resume vector to lowercase (error control)
             vect.push_back(word);
 //            cout << vect[vect.size()-1] << endl; //NOT PERMANENT, NEW
 
@@ -206,8 +256,21 @@ void requirementSet(string userInput, Applicant* applicant){ //Sends pointer of 
     counter++;
     cout << "What is requirement Number " << counter << "? ";
     getline(cin, userInput);
+//    vector<string> splitPut=split(userInput,' ');
     cout << endl;
 //    applicant.setReq(userInput);
+
+//    for(auto elem : splitPut){
+//        applicant->setReq(elem);
+//    }
+    string tempInput;
+    for (int i = 0; i<userInput.size(); i++){ //Sets the input taken from the user to lower case to help with error control
+        tempInput += tolower(userInput[i]);
+    }
+
+    userInput = tempInput;
+    tempInput="";
+
     applicant->setReq(userInput);
 
     do {
